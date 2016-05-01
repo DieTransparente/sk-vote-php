8525aea23581ccbc87b155e855db703fc8cffee7
JossHerrmann <joss.herrmann@gamil.com>
2016-05-01 23:30:08 +0200

    activate translations

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,22 @@
 <?php
 return [
+	'language' => 'de-DE',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+	    'i18n' => [
+	        'translations' => [
+	            'frontend*' => [
+	                'class' => 'yii\i18n\PhpMessageSource',
+	                'basePath' => '@common/messages',
+	            ],
+	            'backend*' => [
+	                'class' => 'yii\i18n\PhpMessageSource',
+	                'basePath' => '@common/messages',
+	            ],
+	        ],
+	    ],
     ],
 ];
